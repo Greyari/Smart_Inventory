f905adcf0e88fbed7852cf414536785bc645deb1
Greyari <159263690+Greyari@users.noreply.github.com>
2026-02-24 10:59:21 +0700

    hapus seeder

--- database/seeders/RiwayatPeminjamanSeeder.php
+++ database/seeders/RiwayatPeminjamanSeeder.php
@@ -12,19 +12,19 @@ class RiwayatPeminjamanSeeder extends Seeder
      */
         public function run()
         {
-            Peminjaman::create([
-                'nama_mahasiswa' => 'Ahmad Hidayat',
-                'nim' => '12345678',
-                'tanggal_peminjaman' => '2024-11-01',
-                'tanggal_pengembalian' => '2024-11-10',
-            ]);
-    
-            Peminjaman::create([
-                'nama_mahasiswa' => 'Putri Ayu',
-                'nim' => '87654321',
-                'tanggal_peminjaman' => '2024-11-05',
-                'tanggal_pengembalian' => null,
-            ]);
+            // Peminjaman::create([
+            //     'nama_mahasiswa' => 'Ahmad Hidayat',
+            //     'nim' => '12345678',
+            //     'tanggal_peminjaman' => '2024-11-01',
+            //     'tanggal_pengembalian' => '2024-11-10',
+            // ]);
+
+            // Peminjaman::create([
+            //     'nama_mahasiswa' => 'Putri Ayu',
+            //     'nim' => '87654321',
+            //     'tanggal_peminjaman' => '2024-11-05',
+            //     'tanggal_pengembalian' => null,
+            // ]);
         }
     }
 
